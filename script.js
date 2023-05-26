693e96422b0c14fd91dcbb7e9c8bd1a53ab471b9
Daniel Rs <charlies1@live.com.mx>
2023-05-26 11:56:53 -0600

    Funcionabilidad y diseño mejorado

--- script.js
+++ script.js
@@ -5,61 +5,85 @@ document.getElementById("copiar").style.display = "none";
 var encriptar = document.getElementById("encriptar");
 var desencriptar = document.getElementById("desencriptar");
 var copiar = document.getElementById("copiar");
+var borrar = document.getElementById("borrar");
+var texto = document.getElementById("mensaje");
+var resultado = document.getElementById("resultado");
+
+borrar.style.display = "none";
+
+texto.onkeydown = mostrar;
+
+function mostrar() {
+    borrar.style.display = "block";
+}
+
 
 encriptar.onclick = encriptacion;
 desencriptar.onclick = desencriptacion;
 copiar.onclick = copiarTexto;
-
-var resultado = document.getElementById("resultado");
+borrar.onclick = borrando
+  
+function borrando(){
+  texto.value = "";
+  borrar.style.display = "none";
+}
 
 function encriptacion() {
     
-    var texto = document.getElementById("mensaje").value;
+    var mensaje = texto.value;
 
-    var patron = /[A-Z]|á|é|í|ó|ú/g;
-    comprobacion = texto.match(patron);
-
-    if (comprobacion == null) {
-        document.getElementById("muneco").style.display = "none";
-        document.getElementById("instruccion").style.display = "none";
-        document.getElementById("resultado").style.display = "block";
-        document.getElementById("copiar").style.display = "block";
-
-        var mapObj = {e:"enter", i:"imes", a:"ai", o:"ober", u:"ufat"};
-        var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
-        texto = texto.replace(re, function(matched){return mapObj[matched];});
-
-        resultado.value = texto;
+    if(mensaje == "") {
+        Swal.fire('No se puede encriptar un mensaje vacío')
     }
-    else {
-        window.alert("No se puede encriptar letras mayúsculas ni acentos");
+    else {  
+        var patron = /[A-Z]|á|é|í|ó|ú/g;
+        comprobacion = mensaje.match(patron);
+
+        if (comprobacion == null) {
+            document.getElementById("muneco").style.display = "none";
+            document.getElementById("instruccion").style.display = "none";
+            document.getElementById("resultado").style.display = "block";
+            document.getElementById("copiar").style.display = "block";
+
+            var mapObj = {e:"enter", i:"imes", a:"ai", o:"ober", u:"ufat"};
+            var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
+            mensaje = mensaje.replace(re, function(matched){return mapObj[matched];});
+
+            resultado.value = mensaje;
+        }
+        else {
+            window.alert("No se puede encriptar letras mayúsculas ni acentos");
+        }
     }
-    
 }
 
 function desencriptacion() {
 
-    var texto = document.getElementById("mensaje").value;
+    var mensaje = texto.value;
 
-    var patron = /[A-Z]|á|é|í|ó|ú/g;
-    comprobacion = texto.match(patron);
-
-    if (comprobacion == null) {
-        document.getElementById("muneco").style.display = "none";
-        document.getElementById("instruccion").style.display = "none";
-        document.getElementById("resultado").style.display = "block";
-        document.getElementById("copiar").style.display = "block";
-
-        var mapObj = {enter:"e", imes:"i", ai:"a", ober:"o", ufat:"u"};
-        var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
-        texto = texto.replace(re, function(matched){return mapObj[matched];});
-
-        resultado.value = texto;
+    if (mensaje == "") {
+        Swal.fire('No se puede desencriptar un mensaje vacío')
     }
     else {
-        window.alert("No se puede encriptar letras mayúsculas ni acentos");
+        var patron = /[A-Z]|á|é|í|ó|ú/g;
+        comprobacion = mensaje.match(patron);
+
+        if (comprobacion == null) {
+            document.getElementById("muneco").style.display = "none";
+            document.getElementById("instruccion").style.display = "none";
+            document.getElementById("resultado").style.display = "block";
+            document.getElementById("copiar").style.display = "block";
+
+            var mapObj = {enter:"e", imes:"i", ai:"a", ober:"o", ufat:"u"};
+            var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
+            mensaje = mensaje.replace(re, function(matched){return mapObj[matched];});
+
+            resultado.value = mensaje;
+        }
+        else {
+            window.alert("No se puede encriptar letras mayúsculas ni acentos");
+        }
     }
-    
 }
 
 function copiarTexto() {
